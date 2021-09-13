4fc5fb816e87c5c3890ab0f57bc8fdc1332ba895
knud21kea <grah0070@stud.kea.dk>
2021-09-13 16:33:05 +0200

    Enigma vertical prototype

--- src/uds/knudsoft/Main.java
+++ src/uds/knudsoft/Main.java
@@ -1,6 +1,10 @@
 package uds.knudsoft;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         // Enigma vertical prototype
@@ -10,42 +14,60 @@ public class Main {
         //Encode or decode?
 
         //if encode
-        //input a string - test with "KODE"
-        String inputString = "KODE";
-        //convert to uppercase (method)
+        //input a string
+        String inputString = "abcdZ";
+
+        //convert to uppercase
+        inputString = inputString.toUpperCase();
+        final int inputLength = inputString.length();
+
         //call stringToIntArray()
-        stringToIntArray(inputString);
+        int[] intArray = stringToIntArray(inputString, inputLength);
+
         //call convertListOfNumbersToString
+        String outputString = convertListOfNumbersToString(intArray, inputLength);
+
         //output encoded string
+        System.out.println(outputString);
 
         //else decode
         //input a list of numbers - test with 1,2,3,4
 
     }
-    //String from int array
-
-    //String to int array
-    public static void stringToIntArray(String inputString) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
-        int length = inputString.length();
-        int[] intArray = new int[length];
-        String ch;
-        int index = 0;
-        for (int i = 0; i < length; i++) {
-            ch = inputString.substring(i, i + 1);
-            index = characters.indexOf(ch);
-            intArray[i] = index +1;
-            System.out.println(intArray[i]);
-        }
-    }
 
     //method to convert a string to a list of numbers
+    public static int[] stringToIntArray(String inputString, int inputLength) {
+        int[] intArray = new int[inputLength];
+        String letter;
+        for (int i = 0; i < inputLength; i++) {
+            letter = inputString.substring(i, i + 1);
+            int index = letterToInt(letter);
+            intArray[i] = index;
+        }
+        return intArray;
+    }
 
     //method to convert 1 letter to a number
+    public static int letterToInt(String letter) {
+        String characters = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return characters.indexOf(letter);
+    }
 
     //method to convert a list of numbers to a string
+    public static String convertListOfNumbersToString (int[] intArray, int inputLength) {
+        String outputString = "{ ";
+        for (int i=0;i<inputLength-1;i++) {
+            outputString = outputString + intArray[i] + ", ";
+        }
+        outputString = outputString + intArray[inputLength-1] + " }";
+    return outputString;
+    }
 
     //method to convert 1 number to a letter
+    public static String convertNumberToLetter(int number) {
+        String characters = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return characters.substring(number, number + 1);
+    }
 
     //convert users input to captitals
 
