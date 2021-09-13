fee332ce9fe37a4e1e25bf7a22746a51349e1754
knud21kea <grah0070@stud.kea.dk>
2021-09-13 11:16:51 +0200

    Enigma vertical prototype

---
+++ src/uds/knudsoft/Main.java
@@ -0,0 +1,25 @@
+package uds.knudsoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Enigma vertical prototype
+
+
+        //String from int array
+
+        //String to int array
+
+        //method to convert a string to a list of numbers
+
+        //method to convert 1 letter to a number
+
+        //method to convert a list of numbers to a string
+
+        //method to convert 1 number to a letter
+
+        //convert users input to captitals
+
+
+    }
+}
